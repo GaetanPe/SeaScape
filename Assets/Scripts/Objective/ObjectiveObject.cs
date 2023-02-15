3557cdc152043660e1e9c0150513b491c676601a
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-02-15 14:15:06 +0100

    add singleton class

--- Assets/Scripts/Objective/ObjectiveObject.cs
+++ Assets/Scripts/Objective/ObjectiveObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Objective n", menuName= "ScriptableObject/Objective", order =2)]
-public class ObjectiveObeject : ScriptableObject
+public class ObjectiveObject : ScriptableObject
 {
     [SerializeField] private string objectiveName;
     [SerializeField] private int goldReward;
--- Assets/Scripts/Objective/ObjectiveSystem.cs
+++ Assets/Scripts/Objective/ObjectiveSystem.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Engine.Utils;
 
-public class ObjectiveSystem : MonoBehaviour
+public class ObjectiveSystem : Singleton<ObjectiveSystem>
 {
-       [SerializeField] private List<ObjectiveObeject> objectiveObejects= new List<ObjectiveObeject>();
+       [SerializeField] private List<ObjectiveObject> objectiveObejects= new List<ObjectiveObject>();
 
 }
