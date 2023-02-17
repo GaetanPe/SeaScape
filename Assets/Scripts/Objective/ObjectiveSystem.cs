1ae328e549e5b40d964fb49be24fd67454ad84b3
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-02-17 16:10:38 +0100

    update

--- Assets/Scripts/Objective/ObjectiveSystem.cs
+++ Assets/Scripts/Objective/ObjectiveSystem.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Engine.Utils;
+using System;
 
 public class ObjectiveSystem : Singleton<ObjectiveSystem>
 {
-       [SerializeField] private List<ObjectiveObject> objectiveObejects= new List<ObjectiveObject>();
+    [SerializeField] private List<ObjectiveObject> objectiveObejects= new List<ObjectiveObject>();
+    System.Random randomObjective = new System.Random();
+    int nbrRandom = 0;
+
+    void currentObjective()
+    {
+        nbrRandom = randomObjective.Next(objectiveObejects.Count);
+
+    }
+
+   
 
 }
