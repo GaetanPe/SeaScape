37df00c3710b159eed346c5d350228ddb27447a8
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-03-09 20:28:49 +0100

    pauseMenu is allready

---
+++ Assets/Scripts/Canvas/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    private void LateUpdate()
+    {
+        if (pausePanel == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape)) 
+            {
+                pausePanel.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+    public void ButtonPause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+
+    public void closeWindow()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void MenuScene()
+    {
+        closeWindow();
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
